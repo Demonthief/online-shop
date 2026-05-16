8509129794d069667d8bd5978739174901daff48
Demonthief <88440274+Demonthief@users.noreply.github.com>
2026-05-16 16:35:13 +0700

    Update api.js

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : import.meta.env.VITE_API_URL
+    baseURL : "https://online-shop-production-b929.up.railway.app"
 })
 
 api.interceptors.request.use(
