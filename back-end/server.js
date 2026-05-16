c7986ccca9cd4d018e791f758e59896b8904188f
Demonthief <88440274+Demonthief@users.noreply.github.com>
2026-05-16 17:27:17 +0700

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -9,7 +9,8 @@ dotenv.config();
 
 const app = express()
 
-app.use(cors());
+app.use(cors({
+    origin : origin: process.env.CORS));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}))
 app.use("/api/auth", authRoutes)
